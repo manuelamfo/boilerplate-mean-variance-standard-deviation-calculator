71475d8fc475b58ce7664b8aae9fd2fd15cdb640
manuela oliveira <manuelamfo@gmail.com>
2025-08-25 17:19:57 +0000

    initial resolution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def calculate(list):
+    matrix = np.array([[list[0:3]],[list[3:6]],[list[6:9]]])
+    mean_x_axis_matrix = matrix.mean(axis=0)
+    mean_y_axis_matrix = matrix.mean(axis=1)
+    mean_matrix = matrix.mean()
 
+    print(f"mean x: {mean_x_axis_matrix}, mean y:{mean_y_axis_matrix}, mean across x and y:{mean_matrix}\n")
 
-
-
+    calculations = {'mean': [mean_x_axis_matrix], [mean_y_axis_matrix], [mean_matrix]}
     return calculations
